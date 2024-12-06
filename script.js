02de8b50a6ecde82264d7dcb8ebe6518b4f96dda
Zaimabintezia <Zaimazia68@gmail.com>
2024-12-06 12:27:25 +0600

    JS intro done.

---
+++ script.js
@@ -0,0 +1,23 @@
+document.write("Starting JavaScript...");
+console.log(3 + 4);
+//Variables
+var num1 = 20;
+var num2 = 10;
+var add = num1 + num2;
+
+document.getElementById
+    ('add_result').innerHTML = add;
+
+//Datatypes
+var a = 300; //Number
+var b = 4.5; //Number
+var c = 'Javascript'; //String
+var d = false // Boolean
+
+// Student Info
+var student_name = "Zaima";
+var student_grade = 8;
+var favorite_subject = 'Science';
+var student_information = `My name is ${student_name}. I study in grade ${student_grade}. My favorite subject is ${favorite_subject}.`;
+
+document.getElementById('student_info').innerHTML = student_information;
